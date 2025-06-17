dc8e2fb8b22cdc47281b731e124310a5ec9fa920
GitHubWestie <127797224+GitHubWestie@users.noreply.github.com>
2025-06-17 22:32:44 +0100

    10. Model Factories

--- example/app/Models/Job.php
+++ example/app/Models/Job.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Job extends Model // Extending to Model gives access to all of it's methods
 {
+    use HasFactory;
+    
     protected $table = 'job_listings'; // Tells Eloquent the table name if the class name doesn't match
 
     protected $fillable = [
