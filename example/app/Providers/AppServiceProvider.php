76d0aaf15c15de9f17a53e788ae9cdcabf7c5d6f
GitHubWestie <127797224+GitHubWestie@users.noreply.github.com>
2025-07-02 07:14:52 +0100

    23. Six Steps to Authorization Mastery

--- example/app/Providers/AppServiceProvider.php
+++ example/app/Providers/AppServiceProvider.php
@@ -3,8 +3,10 @@
 namespace App\Providers;
 
 use App\Models\Job;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 // use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,7 +25,13 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Model::preventLazyLoading();
-        
+
+        // Defines a new gate that determines if the current user has permission to edit a job. Returns a boolean.
+        // Gate::define('edit-job', function (User $user, Job $job) :bool
+        // {
+        //     return $job->employer->user->is($user);
+        // });
+
         // Model::unguard();
         // Paginator::useBootstrapFive(); Just an example of changing the default
     }
